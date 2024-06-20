2fab49aa9919590794de3e75654cfdb897be6640
shootergamer234 <giuseppe2004.cg@gmail.com>
2024-06-20 20:35:56 +0200

    Small adjustments to stub methods in download.js

--- script/downloader.js
+++ script/downloader.js
@@ -12,48 +12,48 @@ export function startDownload(dl_opt) {
     if (dl_opt.file_ext == "mp4") 
         downloadMP4s(links);
     else if (dl_opt.file_ext == "mp3")
-        downloadMP3s(links);
+        downloadMP3s(links, dl_opt.embed_metadata, dl_opt.metadata_type);
     else if (dl_opt.file_ext == "ogg")
-        downloadOGGs(links);
+        downloadOGGs(links, dl_opt.embed_metadata, dl_opt.metadata_type);
 }
 
 /**
- * Downloads videos in mp4 format
- * @param {Array} links - Links to download
+ * Downloads videos in mp4 format.
+ * @param {Array} links - Links to download.
  */
 function downloadMP4s(links) {
-
+    
 }
 /**
- * Downloads music in mp3 format
- * @param {Array} links - Links to download
- * @param {boolean} embed_metadata - Whether or not to embed metadata
- * @param {string} metadata_type - Type of metadata embedding
+ * Downloads music in mp3 format with or without metadata.
+ * @param {Array} links - Links to download.
+ * @param {boolean} embed_metadata - Whether or not to embed metadata.
+ * @param {string} metadata_type - Type of metadata embedding.
  */
 function downloadMP3s(links, embed_metadata, metadata_type) {
 
 }
 /**
- * Downloads music in ogg format
- * @param {Array} links - Links to download
- * @param {boolean} embed_metadata - Whether or not to embed metadata
- * @param {string} metadata_type - Type of metadata embedding
+ * Downloads music in ogg format with or without metadata.
+ * @param {Array} links - Links to download.
+ * @param {boolean} embed_metadata - Whether or not to embed metadata.
+ * @param {string} metadata_type - Type of metadata embedding. Is ignored if embed_metadata is false
  */
 function downloadOGGs(links, embed_metadata, metadata_type) {
 
 }
 /**
- * Find an equavalant mp3 link to a webm link
- * @param {string} link - Link in webm format to find an equivalent link to
- * @returns {string} Equivalent mp3 link
+ * Find an equavalant mp3 link to a webm link.
+ * @param {string} link - Link in webm format to find an equivalent link to.
+ * @returns {string} Equivalent mp3 link.
  */
 function getMP3Address(link) {
 
 }
 /**
- * Find an equavalant ogg link to a webm link
- * @param {string} link - Link in webm format to find an equivalent link to
- * @returns {string} Equivalent ogg link
+ * Find an equavalant ogg link to a webm link.
+ * @param {string} link - Link in webm format to find an equivalent link to.
+ * @returns {string} Equivalent ogg link.
  */
 function getOGGAddress(link) {
 
